755dc391c47813d3b98e1dff10a11a42b0a3dfd0
[iecargnino] <[iecargnino@gmail.com]>
2021-11-27 14:57:44 -0300

    noError... añadido en production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebPackPlugin    = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const CopyPlugin              = require('copy-webpack-plugin');
+const CopyPlugin           = require('copy-webpack-plugin');
 
 module.exports = {
 
